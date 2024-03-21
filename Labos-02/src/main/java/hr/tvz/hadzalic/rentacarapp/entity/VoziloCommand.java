a1015e645295b43d730e268d8af7ae51ba1a0877
Sanjin Hadzalic <sh14.sanjin@yahoo.com>
2024-03-21 15:15:21 +0100

    feature/added rest of validation for VoziloCommand

--- Labos-02/src/main/java/hr/tvz/hadzalic/rentacarapp/entity/VoziloCommand.java
+++ Labos-02/src/main/java/hr/tvz/hadzalic/rentacarapp/entity/VoziloCommand.java
@@ -1,9 +1,6 @@
 package hr.tvz.hadzalic.rentacarapp.entity;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
@@ -27,16 +24,25 @@ public class VoziloCommand {
     @PositiveOrZero(message = "Max number of doors must be a positive number")
     private Integer numberOfDoors;
 
+    @NotNull(message = "Fuel type must not be null")
     private String fuelType;
 
+    @NotBlank(message = "Last service date must not be blank")
     private LocalDate lastServiceDate;
 
+    @NotBlank(message = "Next service date must not be blank")
     private LocalDate nextSeviceDate;
 
+    @NotNull(message = "Mileage must not be null")
+    @PositiveOrZero(message = "Mileage must be a positive number")
     private Double mileage;
 
+    @NotEmpty(message = "Registration must not be empty")
+    @Pattern(regexp = "-", message = "Registration must contain a hyphen")
     private String registration;
 
+    @NotNull(message = "Vehicle Identification Number must not be null")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Vehicle Identification Number must contain only numbers (0-9) or characters (a-z or A-Z)")
     private String vehicleIdentificationNumber;
 
 }
