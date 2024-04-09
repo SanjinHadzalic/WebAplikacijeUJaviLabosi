769da664ad1f922c86f6b6517bf2603b0a746dee
Sanjin Hadzalic <sh14.sanjin@yahoo.com>
2024-04-09 22:17:02 +0200

    feature/implemented Reactive Form Validation for from creation of new Vozilo object

--- Labos-04/frontend/src/app/components/vozilo/vozilo.component.ts
+++ Labos-04/frontend/src/app/components/vozilo/vozilo.component.ts
@@ -3,12 +3,13 @@ import { Vozilo } from '../../interfaces/vozilo';
 import { VoziloService } from '../../services/vozilo.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { regExpEscape } from '@ng-bootstrap/ng-bootstrap/util/util';
 
 @Component({
   selector: 'app-vozilo',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './vozilo.component.html',
   styleUrl: './vozilo.component.css',
   providers: [VoziloService]
@@ -19,6 +20,44 @@ export class VoziloComponent implements OnInit{
   newVozilo!: Vozilo;
 
   constructor(private voziloService: VoziloService, private router: Router) {}
+
+  voziloForm = new FormGroup({
+    registration: new FormControl("", [
+      Validators.required,
+      Validators.pattern(/^[A-Z0-9]+ [-] [A-Z0-9]+$/)
+    ]),
+    vin: new FormControl("", [
+      Validators.required,
+      Validators.pattern(/^[a-zA-Z0-9]+$/)
+    ]),
+    maxNumberOfPassenger: new FormControl("", [
+      Validators.required,
+      Validators.max(6)
+    ]),
+    shifter: new FormControl("", [
+      Validators.required
+    ]),
+    airConditioning: new FormControl("", [
+      Validators.required
+    ]),
+    numberOfDoors: new FormControl("", [
+      Validators.required,
+      Validators.max(5)
+    ]),
+    fuelType: new FormControl("", [
+      Validators.required
+    ]),
+    lastServiceDate: new FormControl("", [
+      Validators.required
+    ]),
+    nextServiceDate: new FormControl("", [
+      Validators.required
+    ]),
+    mileage: new FormControl("", [
+      Validators.required,
+      Validators.min(0)
+    ]) 
+  })
 
   ngOnInit() : void {
     this.getVozila();
@@ -56,17 +95,31 @@ export class VoziloComponent implements OnInit{
   }
 
   saveVozilo() {
+    const formValue = this.voziloForm.value;
+  
+    this.newVozilo.registration = formValue.registration!;
+    this.newVozilo.vin = formValue.vin!;
+    this.newVozilo.maxNumberOfPassenger = +formValue.maxNumberOfPassenger!;
+    this.newVozilo.shifter = formValue.shifter!;
+    this.newVozilo.airConditioning = formValue.airConditioning!;
+    this.newVozilo.numberOfDoors = +formValue.numberOfDoors!;
+    this.newVozilo.fuelType = formValue.fuelType!;
+    this.newVozilo.lastServiceDate = new Date(formValue.lastServiceDate!);
+    this.newVozilo.nextServiceDate = new Date(formValue.nextServiceDate!);
+    
+    this.newVozilo.mileage = +formValue.mileage!;
+  
     this.voziloService.createVozilo(this.newVozilo).subscribe({
       next: (data) => {
-        console.log(data)
-        location.reload();
+        console.log(data);
+        location.reload(); 
       },
       error: (e) => {
-        console.log('error:',e)
+        console.log('error:', e);
       }
-    })
+    });
   }
-
+  
   onSubmit() {
     console.log(this.newVozilo)
     this.saveVozilo();
