1866eae1b4416ab6193065b4546c1ea6b17eec3b
Sanjin Hadžalić <sh14.sanjin@yahoo.com>
2024-05-06 08:53:14 +0200

    fix/Fixed POST method for class Vozilo
    - added documentation.md for problem-solving tricks

--- Labos-06/src/main/java/hr/tvz/hadzalic/rentacarapp/entity/Vozilo.java
+++ Labos-06/src/main/java/hr/tvz/hadzalic/rentacarapp/entity/Vozilo.java
@@ -2,16 +2,13 @@ package hr.tvz.hadzalic.rentacarapp.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
 @Entity
-//@Table(name = "vozilo")
 public class Vozilo {
 
     @Id
--- Labos-06/src/main/resources/data.sql
+++ Labos-06/src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO VOZILO(id, registration, vin, max_Number_Of_Passenger, shifter, air_Conditioning, number_Of_Doors, fuel_Type,
+INSERT INTO VOZILO( registration, vin, max_Number_Of_Passenger, shifter, air_Conditioning, number_Of_Doors, fuel_Type,
                    last_Service_Date, next_Service_Date, mileage) VALUES
-                                                                  (1L, 'ZG9891 - GV','2DJRN5DG9ARP44306', 5, 'standard shifter', 'standard A/C', 5, 'diesel',
+                                                                  ('ZG9891 - GV','2DJRN5DG9ARP44306', 5, 'standard shifter', 'standard A/C', 5, 'diesel',
                                                                    '2022-02-02', '2023-02-02', 1000.2),
-                                                                  (2L, 'KA3232 - OM','WP0ZZZ99ZTS392124', 5, 'sport shifter', 'standard A/C', 3, 'biodiesel',
+                                                                  ('KA3232 - OM','WP0ZZZ99ZTS392124', 5, 'sport shifter', 'standard A/C', 3, 'biodiesel',
                                                                    '2022-02-02', '2023-02-02', 21000.2),
-                                                                  (3L, 'DA1234 - BJ', 'SR0ZZZ99ZTS391111', 4, 'basic shifter', 'upgraded A/C', 5, 'electricity',
+                                                                  ('DA1234 - BJ', 'SR0ZZZ99ZTS391111', 4, 'basic shifter', 'upgraded A/C', 5, 'electricity',
                                                                    '2022-02-02', '2023-02-02', 12343.6);
 
 INSERT INTO review (title, text, grade, vozilo_id)
